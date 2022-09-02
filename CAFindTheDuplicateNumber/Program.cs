4b4b4485b7dc574a95642e0f6dc47b489ea2cf88
krmckmz <keremcokmez@gmail.com>
2022-09-03 01:10:39 +0300

    first commit

---
+++ CAFindTheDuplicateNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CAFindTheDuplicateNumber
+{
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        int[] nums =new int[] {1,3,4,2,2};
+        int result = FindDuplicate(nums);
+        Console.WriteLine(result.ToString());
+      }
+
+        public static int FindDuplicate(int[] nums) 
+        {    
+             int slowPointer=0;
+             int fastPointer= 0;
+        
+        while(true)
+        {            
+            if(nums[slowPointer] == nums[nums[fastPointer]])
+              break;
+
+              slowPointer++;
+              fastPointer+=2;
+        }
+        
+        int slowPointer2=0;
+
+        while(true)
+        {
+            slowPointer--;
+            slowPointer2++;
+
+            if(nums[slowPointer] == nums[slowPointer2])            
+               return nums[slowPointer];
+                      
+        }
+        }
+    }
+
+    
+}
